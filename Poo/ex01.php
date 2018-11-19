2a4982ddefc01ee7e0dd9c4e9f5318284f4a6fb4
marcosvanille <marcosvanillle@gmail.com>
2018-11-19 17:24:42 -0200

    [UPDATE]

---
+++ Poo/ex01.php
@@ -0,0 +1,19 @@
+<?php
+
+class Pessoa {
+
+  $nome;  //Atributo
+
+
+public function falar(){//Metodo
+
+    return "O meu nome e".$this->falar();
+}
+
+}
+
+$glaucio = new Pessoa();
+$glaucio->nome = "Glaucio Daniel";
+$glaucio->falar();
+
+
---
+++ Poo/ex02.php
@@ -0,0 +1,53 @@
+<?php
+
+class  carro
+{
+
+    private $modelo;
+    private $motor;
+    private $ano;
+
+    public function getModelo()
+    {
+
+        return $this->modelo;
+
+    }
+
+    public function setModelo($modelo)
+    {
+
+        $this->modelo = $modelo;
+
+    }
+
+    public function getMotor()
+    {
+
+        return $this->motor;
+
+    }
+
+    public function setMotor($motor)
+    {
+
+        $this->motor = $motor;
+
+
+    }
+
+    public function getAno()
+    {
+
+        return $this->ano;
+
+    }
+
+    public function setAno($ano)
+    {
+
+        $this->ano = $ano;
+    }
+}
+
+
