cfec2357a6c345ffa1e3cb5c7628e4052a6532a1
marcos <marcosvanillle@gmail.com>
2018-11-21 09:22:56 -0200

    [UPDATE]

---
+++ sessoes/sessao04.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcos
+ * Date: 30/10/18
+ * Time: 18:01
+ */
---
+++ sessoes/sessao05.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once ('config.php');
+
+echo session_save_path();
+
+echo "<br>";
+
+var_dump(session_status());
