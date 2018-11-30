f11467b5fd5e23703290a7e79ad5c48437574368
marcos <marcosvanillle@gmail.com>
2018-11-30 02:08:48 -0200

    [UPDATE]

--- Poo/autoLoad/ex01.php
+++ Poo/autoLoad/ex01.php
@@ -1,15 +1,15 @@
-<?php
+ <?php
 
  function __autoload($nomeClasse){
 
 //     var_dump($nomeClasse);
      require_once("$nomeClasse.php");
-}
+ }
 
 
 
 
-$carro = new Delrey();
+ $carro = new Delrey();
 
 $carro->acelerar(80);
 
---
+++ Poo/namespace/config.php
@@ -0,0 +1,14 @@
+<?php
+
+spl_autoload_register(function($nameclass){
+
+$dirClass = "class";
+$filename = $dirClass.DIRECTORY_SEPARATOR.$nameclass.".php";
+
+    if(file_exists($filename)){
+
+        require_once ($filename);
+
+    }
+
+});
